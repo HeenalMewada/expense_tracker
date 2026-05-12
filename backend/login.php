7aa78b136fea51a0f81a6cc00b44b63c66752880
HeenalMewada <mewadaheenal304@gmail.com>
2026-05-12 21:04:49 +0530

    fix: vercel routing

--- backend/login.php
+++ backend/login.php
@@ -4,11 +4,7 @@ header("Access-Control-Allow-Origin: https://expense-tracker-six-mu-45.vercel.ap
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// Handle preflight OPTIONS request
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
